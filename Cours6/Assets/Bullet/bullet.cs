ce2ae7a3bc8a802bd25003dd33da814aa0d4c7fd
B533P-23\533 <anthony.cote@outlook.com>
2020-09-21 12:39:17 -0400

    Yo wassup alex

---
+++ Cours6/Assets/Bullet/bullet.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    Rigidbody myRigidBody;
+    public int lifeSpan = 3;
+    public int bulletSpeed = 1;
+    public int bulletDamage = 1;
+    private float timeLeftToLive;
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody>();
+        timeLeftToLive = lifeSpan;
+    }
+
+
+    void Update()
+    {
+        Vector3 position = transform.position;
+        position += transform.forward * Time.deltaTime * bulletSpeed;
+        myRigidBody.MovePosition(position);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        damageableBullet damageableObject = other.GetComponent<damageableBullet>();
+        if (damageableObject != null)
+            damageableObject.TakeDamage(1);
+    }
+}
---
+++ Cours6/Assets/Ennemi/Script/EnnemiControl.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnnemiControl : MonoBehaviour, damageableBullet
+{
+    private GameObject player;
+    private Rigidbody myRigidBody;
+    public void TakeDamage(int damage)
+    {
+        Die();
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myRigidBody.MovePosition(Vector3.MoveTowards(transform.position, player.transform.position, 100 * Time.deltaTime));
+    }
+}
